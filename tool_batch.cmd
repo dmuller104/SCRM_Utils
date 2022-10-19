5daf0a56ae21f29cebc0c3eb7217f38fe004f94f
cellphone <dmuller104@gmail.com>
2022-10-19 13:11:35 -0600

    Fix bug

--- tool_batch.cmd
+++ tool_batch.cmd
@@ -1,6 +1,6 @@
 
-set projname=sbom-tool
-set CLONE_LINK="https://github.com/microsoft/sbom-tool.git"
+set projname=Demo-for-Microsoft-SBOM-Tool
+set CLONE_LINK="https://github.com/adriandiglio/Demo-for-Microsoft-SBOM-Tool.git"
 set thispath=%CD%\%projname%
 set REPO_PATH=%thispath%\%projname%
 set DOCPATH=%thispath%\docs
@@ -12,7 +12,7 @@ git clone %CLONE_LINK% %REPO_PATH% >> %DOCPATH%\%projname%_commands_log.txt
 dir %thispath% /a >> %DOCPATH%\%projname%_commands_log.txt
 dir %REPO_Path% /a >> %DOCPATH%\%projname%_commands_log.txt
 
-sbom-tool.exe generate -bc %REPO_Path%\src -b %REPO_Path% -pn MULLER_PackageName -ps MULLER_PackageSupplier -pv MULLER_PackageVersion_0.0 -nsb http://MULLER_DEREK.sbom >> %DOCPATH%\%projname%_commands_log.txt
+sbom-tool.exe generate -bc %REPO_Path% -b %REPO_Path% -pn MULLER_PackageName -ps MULLER_PackageSupplier -pv MULLER_PackageVersion_0.0 -nsb http://MULLER_DEREK.sbom >> %DOCPATH%\%projname%_commands_log.txt
 dir %REPO_Path% /a >> %DOCPATH%\%projname%_commands_log.txt
 dir %REPO_Path%\_manifest\spdx_2.2 >> %DOCPATH%\%projname%_commands_log.txt
 
